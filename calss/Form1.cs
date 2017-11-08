7fda1d5152ed7d9b5683ad33aef8ea21a1e539b9
HASSAN ARSHAD <hassanarshad115@gmail.com>
2017-11-08 09:14:05 -0800

    Add project files.

---
+++ calss/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calss
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Method();
+
+        }
+
+        private void Method()
+        {
+            Form2 o = new Form2();
+            o.Text = textBox1.Text;            
+            o.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+                AddNewForm();
+            
+        }
+
+        private void AddNewForm()
+        {
+            Form obj = new Form();
+            obj.Text = "Create By Button";
+            obj.StartPosition = FormStartPosition.CenterScreen;
+
+            TextBox t = TextBoxOne(obj); // txtbox k lye
+            t.Text = textBox1.Text; //txtbox k lye
+
+
+            TextBox t1 = new TextBox();
+            obj.Controls.Add(t1);
+            Point p1 = new Point(30, 100);
+            t1.Location = p1;
+            t1.Width = 150;
+            t1.Text = textBox2.Text;
+
+            Point loc = new Point(0, 32);
+            Label l = new Label();
+            obj.Controls.Add(l);
+            l.Text = "First Name Is =";
+            l.Location = loc;
+
+
+
+
+            obj.Show();
+        }
+
+        private static TextBox TextBoxOne(Form obj)
+        {
+            TextBox t = new TextBox();
+            obj.Controls.Add(t); //nzr ay
+            Point p = new Point(30, 50);
+            t.Location = p; // side with left right r up down
+            t.Width = 150; // width txtbox ki
+
+            Point loc = new Point(0, 73);
+            Label l = new Label();
+            obj.Controls.Add(l);
+            l.Text = "Second Name Is =";
+            l.Location = loc;
+            return t;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (char.IsDigit(ch))
+            {
+                MessageBox.Show("You can Enter only Sting values", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                e.Handled = true;
+            }
+        }
+    }
+}
